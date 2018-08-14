f3ae2188a80d5bd04fc36bf46f105f674e4cbd65
Olga Fandikova <smovzhi2018@yandex.ru>
2018-08-14 10:45:35 +0700

    Add persons

--- wedding-invitations.config.js
+++ wedding-invitations.config.js
@@ -14,5 +14,7 @@ module.exports = [
     {persons: ['Алена']},
     {persons: ['Денис']},
     {persons: ['Ваня', 'Настя']},
-    {persons: ['Рома']}
+    {persons: ['Рома']},
+    {persons: ['Вова']},
+    {persons: ['Женя']}
 ];
